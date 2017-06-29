e46522d86ab660ea12b30f669aa0d4ebbd9c7f7a
Hossman <hossman333@gmail.com>
2017-06-29 16:30:59 -0600

    Replicating bug

--- demo/demo.js
+++ demo/demo.js
@@ -1,17 +1,49 @@
-const {Schema, DOMParser} = require("prosemirror-model")
-const {EditorView} = require("prosemirror-view")
-const {EditorState} = require("prosemirror-state")
-const {schema} = require("prosemirror-schema-basic")
-const {addListNodes} = require("prosemirror-schema-list")
-const {addTableNodes} = require("prosemirror-schema-table")
-const {exampleSetup} = require("prosemirror-example-setup")
+const { Schema, DOMParser, ProsemirrorNode } = require("prosemirror-model");
+const { EditorView } = require("prosemirror-view");
+const { EditorState } = require("prosemirror-state");
+const { schema } = require("prosemirror-schema-basic");
+const { addListNodes } = require("prosemirror-schema-list");
+const { addTableNodes } = require("prosemirror-schema-table");
+const { exampleSetup } = require("prosemirror-example-setup");
 
 const demoSchema = new Schema({
-  nodes: addListNodes(addTableNodes(schema.spec.nodes, "block+", "block"), "paragraph block*", "block"),
+  nodes: addListNodes(
+    addTableNodes(schema.spec.nodes, "block+", "block"),
+    "paragraph block*",
+    "block"
+  ),
   marks: schema.spec.marks
-})
+});
 
-let state = EditorState.create({doc: DOMParser.fromSchema(demoSchema).parse(document.querySelector("#content")),
-                                plugins: exampleSetup({schema: demoSchema})})
+let nodeViews = {
+  link: (node, view, getPos, decorations) => {
+    const container = document.createElement("div");
+    container.style.backgroundColor = "#ff0000";
+    container.innerHTML = "testing";
 
-let view = window.view = new EditorView(document.querySelector(".full"), {state})
+    console.log(container);
+    console.log(container.innerHTML);
+    return { dom: container };
+  },
+  image: (node, view, getPos, decorations) => {
+    const imgContainer = document.createElement("div");
+    const imgDom = document.createElement("img");
+    imgDom.style.border = "1px solid red";
+    imgDom.src = node.attrs.src;
+    imgContainer.appendChild(imgDom);
+    return { dom: imgContainer };
+  }
+};
+
+let state = EditorState.create({
+  doc: DOMParser.fromSchema(demoSchema).parse(
+    document.querySelector("#content")
+  ),
+  plugins: exampleSetup({ schema: demoSchema })
+});
+
+let view = (window.view = new EditorView(document.querySelector(".full"), {
+  state,
+  editable: () => false,
+  nodeViews
+}));
